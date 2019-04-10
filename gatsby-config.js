3f66aa1a6c2ca1a41c2896c03cabc65b58dd3885
Kohei Arai <tobecomebig@gmail.com>
2019-04-09 22:24:52 -0700

    Updated the site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `新井康平`,
+    author: `Kohei Arai`,
+    description: `Kohei Arai's Blog`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `koheiarai94`,
     },
   },
   plugins: [
